dcde13c81c7f89118f33fe92f6c321bf90905f0c
Pavel Taruts <ptrtss@gmail.com>
2022-06-13 21:54:33 +0300

    --- Build scripts commenting and refactoring. Step. Rewriting comments

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,14 +40,18 @@ dependencyManagement {
 publishing {
     publications {
         create<MavenPublication>("jar") {
-            // Плагин java (на базе которого сделан плагин java-library, который мы здесь используем) уже подготовил нам комплект для публикации
-            // Такие заготовленные комплекты для публикации - это называется компоненты
+            // We use the java-library plugin in this project. The java-library is based upon the java plugin.
+            // During the build process, the java plugin creates a so-called component which is a collection of things to publish.
+            // The maven-publish plugin can create publications from components.
+            // that the maven-publish can use. The component is named "java" after the java plugin.
             from(components["java"])
 
+            // Also we use the plugin io.spring.dependency-management.
+            // This plugin enables us not to specify versions manually for those dependencies of the project
+            // that Spring libraries work with.
+            // But by default the dependency versions in the java component are those specified manually.
+            // This configuration is needed to change this default.
             versionMapping {
-                // Если у некой зависимости, в выбранном ее варианте, есть вот такой атрибут
-                //      org.gradle.usage=java-api
-                // то сделать ей версию как в
                 usage("java-api") {
                     fromResolutionResult()
                 }
